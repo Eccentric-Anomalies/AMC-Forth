3e4493ac2b6b69a16c0fb4197185d537f595c68d
Eric Dennison <ericd@netdenizen.com>
2025-04-30 22:06:07 -0400

    version 1.1.0

--- addons/amc_forth/Version.cs
+++ addons/amc_forth/Version.cs
@@ -6,6 +6,6 @@ namespace Forth
     [GlobalClass]
     public partial class Version : Godot.RefCounted
     {
-        public const string Ver = "1.0.5";
+        public const string Ver = "1.1.0";
     }
 }
